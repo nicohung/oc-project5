ebd05918b4dd3a78aca65505e9bf48d3d2b5b311
Nicolas Hung <nicohung@umich.edu>
2024-01-14 08:47:26 -0500

    ATB button working with localStorage
    Cover scenarios:
    1. Empty localStorage
    2. Empty color or quantity values
    3. Merging same color and quantity products.
    4. Adding new products.

--- front/js/product.js
+++ front/js/product.js
@@ -20,29 +20,31 @@ const CTA = document.querySelector('#addToCart');
 let apiRequest = new XMLHttpRequest();
 
 // Create a Promise
-const requestPromise = new Promise((resolve, reject) => {
-    apiRequest.open('GET', 'http://localhost:3000/api/products/' + productID);
+// const requestPromise = new Promise((resolve, reject) => {
+//     apiRequest.open('GET', 'http://localhost:3000/api/products/' + productID);
 
-    apiRequest.onreadystatechange = () => {
-        if (apiRequest.readyState === 4) {
-            // API returns JSON, but the request receives it as text. 
-            const responseClean = JSON.parse(apiRequest.response);
-            resolve(responseClean);
+//     apiRequest.onreadystatechange = () => {
+//         if (apiRequest.readyState === 4) {
+//             // API returns JSON, but the request receives it as text. 
+//             const responseClean = JSON.parse(apiRequest.response);
+//             resolve(responseClean);
 
-        } else if (apiRequest.readyState === 4 && apiRequest.status !== 200) {
-            // Reject the promise if the status is not 200
-            reject(JSON.parse(apiRequest.response));
-        }
-    };
+//         } else if (apiRequest.readyState === 4 && apiRequest.status !== 200) {
+//             // Reject the promise if the status is not 200
+//             reject(JSON.parse(apiRequest.response));
+//         }
+//     };
 
-    apiRequest.send();
-});
+//     apiRequest.send();
+// });
 
 // Use the Promise
-requestPromise
+fetch('http://localhost:3000/api/products/' + productID) 
+    .then(response => response.json())
+
     //then() method is called if the Promise is resolved (successful response)
     .then(response => {
-        console.log(response); // Handle the successful response
+        // console.log(response);
 
         imageContainer.appendChild(image);
         image.src = response.imageUrl;
@@ -72,24 +74,54 @@ requestPromise
 const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
 
+// Add entries to cart Items
 CTA.addEventListener ('click', ($event) => {
     //prevent default behavior when clicked. 
     $event.preventDefault();
 
-    const ATBProduct = {
-        _id: productID,
-        color: colorContainer.value,
-        quantity: quantity.value,
+    if (colorContainer.value !== '' && quantity.value !== '0') {
+
+        if (cartItems.length !== 0) {
+
+            const match = cartItems.find(item => item._id === productID && item.color === colorContainer.value);
+    
+            if (match) {
+                // if it matches, add the quantity in the dropdown to the match quantity.
+                const newQuantity = parseInt(match.quantity) + parseInt(quantity.value);
+                match.quantity = String(newQuantity);
+    
+            } else {
+                const ATBProduct = {
+                    _id: productID,
+                    color: colorContainer.value,
+                    quantity: quantity.value,
+                };
+            
+                cartItems.push(ATBProduct);
+            };
+
+        } else {
+            const ATBProduct = {
+                _id: productID,
+                color: colorContainer.value,
+                quantity: quantity.value,
+            };
+        
+            cartItems.push(ATBProduct);
+        };
+
     };
 
-    const existingItemIndex = cartItems.findIndex(
-        ATBProduct._id === _id && ATBProduct.color === color
-    );
-
-    // console.log(existingItemIndex);
-
-    // cartItems.push(ATBProduct);
     console.log(cartItems);
-    
 
+    localStorage.setItem('cart', JSON.stringify(cartItems));
+    console.log(localStorage);
 });
+
+
+
+
+  
+
+
+// cartItems.filter(())
